cd7048428c5b9d1099dd5d3e848cd7b8fab58c2c
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2022-12-29 05:38:05 -0500

    wip - refactoring loader

--- src/loader.rs
+++ src/loader.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use std::sync::Arc;
 use std::sync::Mutex;
-use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::atomic::{AtomicBool, AtomicUsize, Ordering};
 use workflow_log::*;
 use workflow_wasm::callback::*;
 use js_sys::{Array,Uint8Array};
@@ -11,9 +11,10 @@ use futures::future::{join_all,BoxFuture,FutureExt};
 use workflow_core::lookup::*;
 use crate::error::Error;
 use crate::result::Result;
+use workflow_core::time::*;
 
 pub type Id = u32;
-pub type ModuleMap = HashMap<Id,Arc<Module>>;
+pub type ContentMap = HashMap<Id,Arc<Content>>;
 
 static mut DOCUMENT_ROOT : Option<web_sys::Element> = None;
 
@@ -41,84 +42,103 @@ pub fn root() -> web_sys::Element {
     }
 }
 
-#[allow(dead_code)]
-pub enum Reference {
-    Style,
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum ContentType {
     Module,
     Script,
+    Style,
+}
+
+impl ContentType {
+    pub fn is_js(&self) -> bool {
+        self == &ContentType::Script || self == &ContentType::Module
+    }
+}
+
+#[allow(dead_code)]
+pub enum Reference {
+    Module,
+    Script,
+    Style,
     Export,
 }
 
 #[allow(dead_code)]
 #[derive(Debug, Clone)]
-pub enum ModuleStatus {
+pub enum ContentStatus {
     Loaded,
     Exists,
     Error,
 }
 
-pub struct Module {
+pub struct Content {
+    pub content_type : ContentType,
     pub url : Mutex<Option<String>>,
     pub id : Id,
     pub ident : &'static str,
     pub content: &'static str,
-    pub references: &'static [(Reference, Option<&'static str>, Id)],
+    pub references: Option<&'static [(Reference, Option<&'static str>, Id)]>,
     pub is_loaded : AtomicBool,
 }
 
 // unsafe impl Send for Module {}
 // unsafe impl Sync for Module {}
 
-impl Module {
+impl Content {
 
     pub fn url(&self) -> Option<String> { 
         self.url.lock().unwrap().clone() 
     }
 
+    // fn content(&self, ctx: &Context) -> Result<String> {
     fn content(&self, ctx: &Context) -> Result<String> {
         let mut text = String::new();
 
-        let mut imports = Vec::new();
-        let mut exports = Vec::new();
+        if let Some(references) = &self.references {
+            let mut imports = Vec::new();
+            let mut exports = Vec::new();
 
-        for (kind,what,id) in self.references.iter() {
-            let module = ctx.get(id).ok_or(format!("unable to lookup module `{}`",self.ident))?;
-            let url = module.url().ok_or(format!("[{}] module is not loaded `{}`",self.ident,id))?;
-            match kind {
-                Reference::Style => {
-                    // TODO: import style into the global document
-                    log_info!("TODO: loading style {}", what.unwrap())
-                },
-                Reference::Script => {
-                    // TODO: import style into the global document
-                    log_info!("TODO: loading style {}", what.unwrap())
-                },
-                Reference::Module => {
-                    match what {
-                        Some(detail) => {
-                            imports.push(format!("import {} from \"{}\";\n", detail, url));
-                        },
-                        None => {
-                            imports.push(format!("import \"{}\";\n", url));
+            for (kind,what,id) in references.iter() {
+                let module = ctx.get(id).ok_or(format!("unable to lookup module `{}`",self.ident))?;
+                let url = module.url().ok_or(format!("[{}] module is not loaded `{}`",self.ident,id))?;
+                match kind {
+                    Reference::Style => {
+                        // TODO: import style into the global document
+                        log_info!("referencing style {}", module.ident)
+                    },
+                    Reference::Script => {
+                        // TODO: import style into the global document
+                        log_info!("TODO: loading script {}", id)
+                    },
+                    Reference::Module => {
+                        match what {
+                            Some(detail) => {
+                                imports.push(format!("import {} from \"{}\";", detail, url));
+                            },
+                            None => {
+                                imports.push(format!("import \"{}\";", url));
+                            }
                         }
+                    },
+                    Reference::Export => {
+                        let module = ctx.get(id).ok_or(format!("unable to lookup module `{}`",self.ident))?;
+                        let url = module.url().ok_or(format!("[{}] module is not loaded `{}`",self.ident,id))?;
+                        exports.push(format!("export {} from \"{}\";", what.unwrap(), url));
                     }
-                },
-                Reference::Export => {
-                    let module = ctx.get(id).ok_or(format!("unable to lookup module `{}`",self.ident))?;
-                    let url = module.url().ok_or(format!("[{}] module is not loaded `{}`",self.ident,id))?;
-                    exports.push(format!("export {} from \"{}\";\n", what.unwrap(), url));
                 }
             }
+
+            let imports = imports.join("\n");
+            let exports = exports.join("\n");
+
+            text += &imports;
+            text += &self.content;
+            text += &exports;
+            Ok(text)
+        } else {
+            Ok(self.content.to_string())
         }
 
-        let imports = imports.join("\n");
-        let exports = exports.join("\n");
-
-        text += &imports;
-        text += &self.content;
-        text += &exports;
-
-        Ok(text)
     }
 
     pub fn is_loaded(&self) -> bool {
@@ -128,33 +148,30 @@ impl Module {
     fn load_deps(self : Arc<Self>, ctx: Arc<Context>) -> BoxFuture<'static, Result<()>> {
 
         async move {
-            let futures = self.references
-                .iter()
-                .map(|(kind,what,id)| {
-                    match kind {
-                        Reference::Module | Reference::Script | Reference::Export => {
-                            if let Some(module) = ctx.get(id) {
-                                if !module.is_loaded.load(Ordering::SeqCst) {
-                                    Some(module.load(&ctx))
-                                } else {
-                                    None
-                                }
+            if let Some(references) = &self.references {
+                let futures = references
+                    .iter()
+                    .map(|(_,_,id)| {
+                        if let Some(content) = ctx.get(id) {
+                            if !content.is_loaded.load(Ordering::SeqCst) {
+                                Some(content.load(&ctx))
                             } else {
-                                log_error!("Unable to locate module {}", id);
                                 None
                             }
-                        },
-                        Reference::Style => {
-                            log_warning!("TODO: import stylesheet: {:?}",what);
+                        } else {
+                            log_error!("Unable to locate module {}", id);
                             None
                         }
-                    }
-                })
-                .flatten()
-                .collect::<Vec<_>>();
-            
-            join_all(futures).await;
+                    })
+                    .flatten()
+                    .collect::<Vec<_>>();
+                
+                join_all(futures).await;
 
+                // for future in futures {
+                //     future.await?;
+                // }
+            }
             Ok(())
         }.boxed()
 
@@ -163,42 +180,60 @@ impl Module {
         // Ok(())
     }
 
-    pub async fn load(self : &Arc<Self>, ctx: &Arc<Context>) -> Result<ModuleStatus> {
-        ctx.load_module(self).await
+    pub async fn load(self : &Arc<Self>, ctx: &Arc<Context>) -> Result<ContentStatus> {
+        ctx.load_content(self).await
     }
 
-    fn create_blob_url_for_script(&self, ctx: &Arc<Context>) -> Result<String> {
+    fn create_blob_url(&self, ctx: &Arc<Context>) -> Result<String> {
+
         let content = self.content(ctx)?;
         let args = Array::new_with_length(1);
         args.set(0, unsafe { Uint8Array::view(content.as_bytes()).into() });
         let mut options = web_sys::BlobPropertyBag::new();
         // options.type_("module");
-        options.type_("application/javascript");
+        match self.content_type {
+            ContentType::Module | ContentType::Script => {
+                options.type_("application/javascript");
+            },
+            ContentType::Style => {
+                options.type_("text/css");
+            },
+        }
+
         let blob = Blob::new_with_u8_array_sequence_and_options(&args, &options)?;
         let url = Url::create_object_url_with_blob(&blob)?;
         self.url.lock().unwrap().replace(url.clone());
         Ok(url)
     }
 
-    async fn load_impl(self : &Arc<Self>, ctx: &Arc<Context>) -> Result<ModuleStatus> {
+    async fn load_impl(self : &Arc<Self>, ctx: &Arc<Context>) -> Result<ContentStatus> {
 
         if self.is_loaded() {
-            return Ok(ModuleStatus::Exists);
+            return Ok(ContentStatus::Exists);
         }
         
         self.clone().load_deps(ctx.clone()).await?;
-        log_info!("load ... {}", self.ident);
+        // log_info!("load ... {}", self.ident);
         
         let (sender,receiver) = oneshot();
-        let url = self.create_blob_url_for_script(ctx)?;
-        let ident = self.ident.clone();
+        let url = self.create_blob_url(ctx)?;
+
+            // let ident = self.ident.clone();
         let callback = callback!(move |_event: web_sys::CustomEvent| {
-            log_info!("{} ... done", ident);
+            // log_info!("{} ... done", ident);
             // TODO - analyze event
-            let status = ModuleStatus::Loaded;
+            let status = ContentStatus::Loaded;
             sender.try_send(status).expect("unable to post load event");
         });
-        self.inject_script(&url, &callback)?;
+
+        match &self.content_type {
+            ContentType::Module | ContentType::Script => {
+                self.inject_script(&url, &callback)?;
+            },
+            ContentType::Style => {
+                self.inject_style(&url, &callback)?;
+            }
+        };
         let status = receiver.recv().await.expect("unable to recv() load event");
         self.is_loaded.store(true, Ordering::SeqCst);
         Ok(status)
@@ -210,11 +245,35 @@ impl Module {
     {
         let script = document().create_element("script")?;                    
         script.add_event_listener_with_callback("load", callback.as_ref())?;
-        let content_type = "module";
-        script.set_attribute("module","true")?;
-        script.set_attribute("type",content_type)?;
+
+        match &self.content_type {
+            ContentType::Module => {
+                script.set_attribute("module","true")?;
+                script.set_attribute("type","module")?;
+            },
+            ContentType::Script => {
+                script.set_attribute("type","application/javascript")?;
+            },
+            _ => { panic!("inject_script() unsupported content type `{:?}`", self.content_type) }
+        }
         script.set_attribute("src", url)?;
+        script.set_attribute("id", self.ident)?;
         root().append_child(&script)?;
+        Ok(())
+    }
+
+    fn inject_style<C>(&self, url : &str, callback : &C) -> Result<()>
+    where
+        C: AsRef<js_sys::Function>
+    {
+        let style = document().create_element("link")?;
+        style.add_event_listener_with_callback("load", callback.as_ref())?;
+        style.set_attribute("type","text/css")?;
+        style.set_attribute("rel","stylesheet")?;
+        style.set_attribute("href",&url)?;
+        style.set_attribute("id",self.ident)?;
+        root().append_child(&style)?;
+println!("injecting style `{}`", self.ident);
         Ok(())
     }
 
@@ -222,33 +281,36 @@ impl Module {
 
 
 pub struct Context {
-    pub modules : Arc<ModuleMap>,
-    pub lookup_handler : LookupHandler<Id,ModuleStatus,Error>,
+    pub content : Arc<ContentMap>,
+    pub lookup_handler : LookupHandler<Id,ContentStatus,Error>,
+    pub loaded : AtomicUsize,
 }
 
 impl Context {
 
-    pub fn new(modules : ModuleMap) -> Context {
+    pub fn new(content : ContentMap) -> Context {
         Context {
-            modules : Arc::new(modules),
+            content : Arc::new(content),
             lookup_handler: LookupHandler::new(),
+            loaded : AtomicUsize::new(0),
         }
     }
 
-    pub fn get<'l>(&'l self, id : &Id) -> Option<&'l Arc<Module>> {
-        self.modules.get(id)
+    pub fn get<'l>(&'l self, id : &Id) -> Option<&'l Arc<Content>> {
+        self.content.get(id)
     }
 
-    pub async fn load_module(self: &Arc<Self>, module : &Arc<Module>) -> Result<ModuleStatus> {
+    pub async fn load_content(self: &Arc<Self>, content : &Arc<Content>) -> Result<ContentStatus> {
 
-        if module.is_loaded() {
-            Ok(ModuleStatus::Exists)
+        if content.is_loaded() {
+            Ok(ContentStatus::Exists)
         } else {
         
-            match self.lookup_handler.queue(&module.id).await {
+            match self.lookup_handler.queue(&content.id).await {
                 RequestType::New(receiver) => {
-                    let result = module.load_impl(self).await;
-                    self.lookup_handler.complete(&module.id, result).await;
+                    self.loaded.fetch_add(1,Ordering::SeqCst);
+                    let result = content.load_impl(self).await;
+                    self.lookup_handler.complete(&content.id, result).await;
                     receiver.recv().await?
                 },
                 RequestType::Pending(receiver) => {
@@ -259,6 +321,8 @@ impl Context {
     }
 
     pub async fn load_ids(self : &Arc<Self>, list : &[Id]) -> Result<()> {
+
+        let start = Instant::now();
 
         let futures = list
             .iter()
@@ -284,6 +348,10 @@ impl Context {
                 }
             }
         }
+
+        let elapsed = start.elapsed();
+        let loaded = self.loaded.load(Ordering::SeqCst);
+        log_info!("Loaded {} references in {} msec", loaded, elapsed.as_millis());
 
         Ok(())
     }
